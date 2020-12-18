7d1cd998f97215cab59d59282cbe0fe302fc1af9
asas1asas200 <asas1asas200@gmail.com>
2020-12-18 21:53:08 +0800

    Test commit

--- FPGrwoth.py
+++ FPGrwoth.py
@@ -14,12 +14,16 @@ class FPBase:
 			self.cnt = cnt
 			self.nodes = []
 
-	def __init__(self, filename, frequency):
+	def __init__(self, filename=None, frequency=0, paths=None):
 		self.frequency = frequency
-		self.data = self.__init_data(filename)
-		self.table = self.__create_header_table()
-		self.data = self.__sort_filter_data()
-		self.__create_FP_tree()
+		if self.filename != None:
+			self.data = self.__init_data(filename)
+			self.table = self.__create_header_table()
+			self.data = self.__sort_filter_data()
+			self.__create_FP_tree()
+		else:
+			
+
 		
 	def __init_data(self, filename):
 		with open(filename, 'r') as file:
@@ -60,10 +64,39 @@ class FPBase:
 		for items in self.data:
 			travel(items)
 
+	@staticmethod
+	def print_tree(node, dep):
+		print('|' + '-' * dep + node.name if node.name is not None else '')
+		for i in node.childs.values():
+			FPBase.print_tree(i, dep+1)
+	
+	def __mining(self, table, path, freq_items):
+		def find_root(node, path):
+			if node.parent != self.root:
+				path.append(node.parent.name)
+				find_root(node, path)
 
-class FP(FPBase):
-	def __init__(self, filename, frequency):
-		super().__init__(filename, frequency)
+		def find_prefix(item, table):
+			table_item = table[item]
+			paths = {}
+			for node in table_item.nodes:
+				path = []
+				find_root(node, path)
+				if len(path) > 1:
+					paths[frozenset(path[1:])] = node.cnt
+			return paths
+			
+		lists = table.keys()
+		for item in lists:
+			newFreq = path.copy()
+			newFreq.add(item)
+			prefix_paths = find_prefix(item)
+			freq_items.append(prefix_paths)
+			sub_tree = FPBase(frequency = self.frequency, paths = prefix_paths)
+			if sub_tree.table != None:
+				self.__mining(sub_tree.table, prefix_paths, freq_items)
 
 if __name__ == '__main__':
-	FP('mushroom.dat', 824)
+	x = FPBase('exam.txt',3)
+	x.print_tree(x.root, 0)
+	#FP('mushroom.dat', 813)
